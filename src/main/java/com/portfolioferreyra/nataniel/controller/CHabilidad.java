008060e4465aa812b202c5ec00c1792cd70c4c10
NatanielFerr <nataniel.ferreyra15@gmail.com>
2023-04-15 19:28:38 -0300

    volviendo a koyeb

--- src/main/java/com/portfolioferreyra/nataniel/controller/CHabilidad.java
+++ src/main/java/com/portfolioferreyra/nataniel/controller/CHabilidad.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("habilidades")
-//@CrossOrigin(origins = "https://portfolionatanielferreyra.web.app")
-@CrossOrigin(origins = "https://deploy-springboot-render-l4xg.onrender.com")
+@CrossOrigin(origins = "https://portfolionatanielferreyra.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CHabilidad {
     @Autowired
     SHabilidad sHabilidad;
--- src/main/java/com/portfolioferreyra/nataniel/controller/CRed.java
+++ src/main/java/com/portfolioferreyra/nataniel/controller/CRed.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("redes")
-//@CrossOrigin(origins = "https://portfolionatanielferreyra.web.app")
-@CrossOrigin(origins = "https://deploy-springboot-render-l4xg.onrender.com")
+@CrossOrigin(origins = "https://portfolionatanielferreyra.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CRed {
     @Autowired
     SRed sRed;
