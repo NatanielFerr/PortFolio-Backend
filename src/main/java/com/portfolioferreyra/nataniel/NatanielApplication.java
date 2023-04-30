b58c536dbe5f570710f65826809b57f2f5d3cffd
NatanielFerr <nataniel.ferreyra15@gmail.com>
2023-04-29 22:38:35 -0300

    finalizado portfolio tp final backend

--- src/main/java/com/portfolioferreyra/nataniel/NatanielApplication.java
+++ src/main/java/com/portfolioferreyra/nataniel/NatanielApplication.java
@@ -9,15 +9,5 @@ public class NatanielApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NatanielApplication.class, args);
 	}
-        
-//        @Bean
-//        public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-//			}
-//		};
-//	}
 
 }
--- src/main/java/com/portfolioferreyra/nataniel/controller/CPersona.java
+++ src/main/java/com/portfolioferreyra/nataniel/controller/CPersona.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,29 +54,6 @@ public class CPersona {
         persoServ.borrarPersona(id);
         return "La persona fue eliminada correctamente";
     }
-    
-//    @PutMapping ("editar/persona/{id}")
-//    public Persona editarPersona(@PathVariable Long id, 
-//                                 @RequestParam("nombre") String nuevoNombre, 
-//                                 @RequestParam("apellido") String nuevoApellido, 
-//                                 @RequestParam("descargacv") String nuevaDescargacv,
-//                                 @RequestParam("titulo") String nuevoTitulo,
-//                                 @RequestParam("sobre_mi") String nuevoSobre_mi,
-//                                 @RequestParam("banner") String nuevoBanner,
-//                                 @RequestParam("imagen") String nuevaImagen){
-//        Persona perso = persoServ.buscarPersona(id);
-//        
-//        perso.setApellido(nuevoApellido);
-//        perso.setNombre(nuevoNombre);
-//        perso.setDescargacv(nuevaDescargacv);
-//        perso.setTitulo(nuevoTitulo);
-//        perso.setSobre_mi(nuevoSobre_mi);
-//        perso.setBanner(nuevoBanner);
-//        perso.setImagen(nuevaImagen);
-//        
-//        persoServ.crearPersona(perso);
-//        return perso;
-//    }
     
     @PutMapping("editar/persona/{id}")
     public void editarPersona(@RequestBody Persona per) {
